546f768c9af6a3502439f8d45698d67bffad0262
Nada El Annasi <157706609+elannasinada@users.noreply.github.com>
2025-11-24 19:53:29 +0100

    Time: 21 ms (24.34%) | Memory: 80.8 MB (28.19%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int i=0; i< nums.length; i++){
+            set.add(nums[i]);
+        }
+        if (set.size() == nums.length){
+            return false;
+        } else{
+            return true;
+        }
+
+    }
+}
