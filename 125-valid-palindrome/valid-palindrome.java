82d4464b9fecd3155b34564ec0e6671002f1a87e
Nada El Annasi <157706609+elannasinada@users.noreply.github.com>
2025-11-25 08:45:06 +0100

    Time: 2 ms (98.64%) | Memory: 44.1 MB (60.74%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.java
+++ 125-valid-palindrome/valid-palindrome.java
@@ -1,19 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-    int i = 0 ; int j= s.length()-1;
-    while (i<j){
-        while (i<j && !Character.isLetterOrDigit(s.charAt(i))){
+        int i= 0; int j= s.length()-1;
+        while (i<j){
+            while (i<j && !alphaNum(s.charAt(i))){
+                i++;
+            }
+            while (i<j && !alphaNum(s.charAt(j))){
+                j--;
+            }
+            if ( (Character.toLowerCase(s.charAt(i)) != (Character.toLowerCase(s.charAt(j)) ))){
+                return false;
+            }
             i++;
-        }
-        while (i<j && !Character.isLetterOrDigit(s.charAt(j))){
             j--;
         }
-        if (Character.toLowerCase(s.charAt(i))!=Character.toLowerCase(s.charAt(j))){
-            return false;
-        }
-        i++;
-        j--;
+        return true;
     }
-    return true;
+    public boolean alphaNum(char c){
+        return (c >= 'A' && c <= 'Z' ||
+                c >= 'a' && c <= 'z' ||
+                c >= '0' && c <= '9');
     }
-    }
+}
